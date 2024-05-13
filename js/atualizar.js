87fb31ab4391804d01e612b02ce00e4d52f6f8a8
Fenixz666 <jppcamargo2@outlook.com>
2024-05-13 14:38:05 -0300

    draw do java

--- js/atualizar.js
+++ js/atualizar.js
@@ -1,21 +1,83 @@
+function calcularMediaProgresso() {
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+    let totalProgresso = 0;
+
+    tarefas.forEach(tarefa => {
+        totalProgresso += tarefa.status;
+    });
+
+    if (tarefas.length > 0) {
+        return totalProgresso / tarefas.length;
+    } else {
+        return 0;
+    }
+}
+
+function atualizarProgressoTotal() {
+    const progressoTotal = calcularMediaProgresso();
+    const progressoTotalElement = document.querySelector("#progresso-total");
+    if (progressoTotalElement) {
+        progressoTotalElement.value = progressoTotal;
+    }
+}
+
 function inc(id){
-
-    const tarefas = JSON.parse ( localStorage.getItem("tarefas") )
-    let tarefa = tarefas.find ( tarefa => tarefa.id = id)    
-    tarefa.status += 10
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+    let tarefa = tarefas.find(tarefa => tarefa.id === id);
     
-    localStorage.setItem("tarefas", JSON.stringify( tarefas) )
+    if (tarefa.status === 100) {
+        return;
+    }
 
-    document.querySelector("#" + id + " progress").value += 10
+    tarefa.status += 10;
+    if (tarefa.status > 100) {
+        tarefa.status = 100;
+    }
+    
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+
+    const progressoElement = document.querySelector("#" + id + " progress");
+    if (progressoElement) {
+        progressoElement.value += 10;
+    }
+
+    atualizarProgressoTotal();
 }
 
 function dec(id){
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+    let tarefa = tarefas.find(tarefa => tarefa.id === id);
+    tarefa.status -= 10;
+    if (tarefa.status < 0) tarefa.status = 0;
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
 
-    const tarefas = JSON.parse ( localStorage.getItem("tarefas") )
-    let tarefa = tarefas.find ( tarefa => tarefa.id = id)    
-    tarefa.status += 10
-    if (tarefa.status <0) tarefa.status = 0
-    localStorage.setItem("tarefas", JSON.stringify( tarefas) )
+    const progressoElement = document.querySelector("#" + id + " progress");
+    if (progressoElement) {
+        progressoElement.value -= 10;
+    }
 
-    document.querySelector("#" + id + " progress").value -= 10
+    atualizarProgressoTotal();
+}
+
+let tarefasCarregadas = false; 
+
+document.addEventListener("DOMContentLoaded", function() {
+    if (!tarefasCarregadas) {
+        carregarTarefas();
+        tarefasCarregadas = true;
+    }
+});
+
+function carregarTarefas() {
+    const listaTarefas = document.querySelector("#lista-de-tarefas");
+    listaTarefas.innerHTML = ''; 
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+    tarefas.forEach(tarefa => {
+        card(tarefa);
+        const progressoElement = document.querySelector("#" + tarefa.id + " progress");
+        if (progressoElement) {
+            progressoElement.value = tarefa.status;
+        }
+    });
+    atualizarProgressoTotal();
 }
